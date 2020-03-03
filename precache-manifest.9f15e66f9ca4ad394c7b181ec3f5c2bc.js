efd4f2586a72a57bd0d1ed265b6379f8dae7ce57
Daniel <donelmalka@gmail.com>
2020-03-03 20:53:24 +0200

    Updates

---
+++ precache-manifest.9f15e66f9ca4ad394c7b181ec3f5c2bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e497cf474de5980a4a569633b0f2cfa",
+    "url": "/SmartHouse2020/index.html"
+  },
+  {
+    "revision": "4e8e8a7deeee3e248169",
+    "url": "/SmartHouse2020/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "efa04066fcdf76769fd1",
+    "url": "/SmartHouse2020/static/css/main.3c2a1a00.chunk.css"
+  },
+  {
+    "revision": "4e8e8a7deeee3e248169",
+    "url": "/SmartHouse2020/static/js/2.1c9cf6df.chunk.js"
+  },
+  {
+    "revision": "73eb153a37d91a1b128cd7c9919e3cef",
+    "url": "/SmartHouse2020/static/js/2.1c9cf6df.chunk.js.LICENSE"
+  },
+  {
+    "revision": "efa04066fcdf76769fd1",
+    "url": "/SmartHouse2020/static/js/main.5ea7ac98.chunk.js"
+  },
+  {
+    "revision": "ea1afa6b2936d006b426",
+    "url": "/SmartHouse2020/static/js/runtime-main.f1de979b.js"
+  }
+]);
